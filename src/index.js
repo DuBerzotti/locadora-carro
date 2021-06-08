12e9e33d72808aa47ceb7ced690331873e575c16
Eduardo Berzotti <eduardoberzotti@hotmail.com>
2021-06-07 21:27:56 -0300

    Criação de primeira rota

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
+
+app.get("/ignite", (request, response) => {
+    return response.json({message: "Hello Word!"});
+})
 
 app.listen(3333);
