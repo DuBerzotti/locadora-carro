8e4c1a5586fd4025a52b573da5fad442494b238c
Eduardo Berzotti <eduardoberzotti@hotmail.com>
2021-06-08 22:49:12 -0300

    Criando cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const { request, response, json } = require('express');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
+
+//Array de de usuários/contas
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+    
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement : []
+    });
+
+    return response.status(201).send();
+
+});
 
 app.get("/courses", (request, response) => {
 
@@ -12,15 +32,6 @@ app.get("/courses", (request, response) => {
     console.log(query);
 
     return response.json(["Curso 1", "Curso 2", "Curso 3"]);
-});
-
-app.post("/courses", (request, response) => {
-
-    const body = request.body;
-
-    console.log(body);
-
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (request, response) => {
