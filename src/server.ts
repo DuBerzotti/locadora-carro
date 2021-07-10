3dba922ef5dc56527e7bd9aa7e4fbc8e6f62b029
Eduardo Berzotti <eduardoberzotti@hotmail.com>
2021-07-10 14:00:44 -0300

    Criado repostorio de especificacao

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express, { request } from 'express';
+
 import {categoriesRoutes} from './Routes/categories.routes';
+import { specificationsRoutes } from './Routes/specification.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
