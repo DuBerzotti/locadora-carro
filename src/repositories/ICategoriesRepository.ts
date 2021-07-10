101b814ef35044e6f2d33f6f62fcacff246f3d30
Eduardo Berzotti <eduardoberzotti@hotmail.com>
2021-07-10 12:25:06 -0300

    Utilizando o principio da substituicao de Liskov

--- src/repositories/ICategoriesRepository.ts
+++ src/repositories/ICategoriesRepository.ts
@@ -1,12 +1,15 @@
 import { Category } from "../model/Category";
 
-
-
+// DTO => Data transfer object
+interface ICreateCategoryDTO {
+    name: string;
+    description: string;
+}
 interface ICategoriesRepository {
     findByName(name: string): Category;
     list(): Category[];
-    create(name: string, description:string): void;
+    create({name, description}: ICreateCategoryDTO): void;
 }
 
 
-export {ICategoriesRepository};
+export {ICategoriesRepository, ICreateCategoryDTO};
