ad154f66383003b239ed18f63281d20e1885dd86
Eduardo Berzotti <eduardoberzotti@hotmail.com>
2021-12-13 21:11:28 -0300

    Alterando a importação dos repositórios

--- src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -1,34 +1,36 @@
+/* eslint-disable indent */
+import { User } from '@modules/accounts/infra/typeorm/entities/user';
+
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
-import { User } from '../../infra/typeorm/entities/user';
 import { IUsersRepository } from '../IUserRepository';
 
 class UsersRepositoryInMemory implements IUsersRepository {
     users: User[] = [];
 
     async create({
-      driver_license,
-      email,
-      name,
-      password,
-    }: ICreateUserDTO): Promise<void> {
-      const user = new User();
-
-      Object.assign(user, {
         driver_license,
         email,
         name,
         password,
-      });
+    }: ICreateUserDTO): Promise<void> {
+        const user = new User();
 
-      this.users.push(user);
+        Object.assign(user, {
+            driver_license,
+            email,
+            name,
+            password,
+        });
+
+        this.users.push(user);
     }
 
     async findByEmail(email: string): Promise<User> {
-      return this.users.find((user) => user.email === email);
+        return this.users.find((user) => user.email === email);
     }
 
     async findById(id: string): Promise<User> {
-      return this.users.find((user) => user.id === id);
+        return this.users.find((user) => user.id === id);
     }
 }
 
--- src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -1,26 +1,28 @@
-import { Category } from '../../infra/typeorm/entities/Category';
+/* eslint-disable indent */
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
+
 import { ICategoriesRepository, ICreateCategoryDTO } from '../ICategoriesRepository';
 
 class CategoriesRepositoryInMemory implements ICategoriesRepository {
     categories: Category[] = [];
 
     async findByName(name: string): Promise<Category> {
-      const category = this.categories.find((category) => category.name === name);
-      return category;
+        const category = this.categories.find((category) => category.name === name);
+        return category;
     }
     async list(): Promise<Category[]> {
-      const all = this.categories;
-      return all;
+        const all = this.categories;
+        return all;
     }
     async create({ name, description }: ICreateCategoryDTO): Promise<void> {
-      const category = new Category();
+        const category = new Category();
 
-      Object.assign(category, {
-        name,
-        description,
-      });
+        Object.assign(category, {
+            name,
+            description,
+        });
 
-      this.categories.push(category);
+        this.categories.push(category);
     }
 }
 
